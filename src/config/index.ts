67a937240464b5fff62f1e315d595b8a5dadecd7
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2023-06-02 19:28:20 +0600

    [DONE] Setup Logger

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,11 +2,15 @@ import dotenv from "dotenv";
 import path from "path";
 
 dotenv.config({
+    // eslint-disable-next-line no-undef
     path: path.join(process.cwd(), ".env"),
 });
 
 export default {
+    // eslint-disable-next-line no-undef
     port: process.env.PORT,
+    // eslint-disable-next-line no-undef
     database_url: process.env.DATABASE_URL,
+    // eslint-disable-next-line no-undef
     default_user_password: process.env.DEFAULT_USER_PASSWORD,
 };
